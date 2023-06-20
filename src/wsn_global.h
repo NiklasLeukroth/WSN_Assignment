1c97ce083abd676a546c13ed170675bce015bc76
NiklasLeukroth <niklasleukroth@gmail.com>
2023-06-20 11:53:51 +0200

    Resolve merge conflict

--- src/wsn_global.h
+++ src/wsn_global.h
@@ -1,14 +1,19 @@
 #ifndef WSN_GLOBAL
 #define WSN_GLOBAL
 
-typedef struct _data_package{
-    char ack;
-    char seq;
-    char payload[30];
+typedef struct _data_package {
+  char ack;
+  char seq;
+  char payload[30];
 } data_package;
 
-typedef struct _short_package{
-    char ack;
-    char seq;
+typedef struct _short_package {
+  char ack;
+  char seq;
 } short_package;
-#endif //WSN_GLOBAL
+
+void print_data_package(data_package *pck, uint16_t datalen) {
+    printf("data package:\n\tACK: %u\n\tSEQ: %u\n\tPAYLOAD: %.*s\n", pck->ack, pck->seq, datalen-2, pck->payload);
+}
+
+#endif // ifndef WSN_GLOBAL
