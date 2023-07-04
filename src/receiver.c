c86fe36eadc74ea9e6e494a716b6ee08de76579c
Konstantin Kochs <konstantin.kochs@mailbox.tu-dresden.de>
2023-07-04 09:57:43 +0200

    switched back to simple-udp

--- src/receiver.c
+++ src/receiver.c
@@ -1,9 +1,10 @@
-#define ZOUL_CONF_USE_CC1200_RADIO 1
+#define ZOUL_CONF_USE_CC1200_RADIO 0
 
-#include "contiki.h"
+#include <stdlib.h>
 
+#include "net/routing/routing.h"
 #include "net/netstack.h"
-// #include "arch/dev/radio/cc1200/cc1200.c"
+#include "net/ipv6/simple-udp.h"
 
 #include "wsn_global.h"
 
@@ -12,36 +13,47 @@
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
+// communication
+#define RECEIVER_PORT	8765
+#define SENDER_PORT	5678
+static struct simple_udp_connection udp_conn;
+
 PROCESS(receiver_process, "receiver process");
 AUTOSTART_PROCESSES(&receiver_process);
 
-void rx_callback() {
-  int pkg;
+static void udp_rx_callback(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
+  data_package *pck = (data_package *) data;
+  LOG_INFO_6ADDR(sender_addr);
+  printf(" => \n");
 
-  // NETSTACK_RADIO.read(&pkg, sizeof(data_package));
-  NETSTACK_RADIO.read(&pkg, sizeof(int));
+  if (pck->ack == 0x00) {
+    print_data_package(pck, datalen);
 
-  printf("received ");
-  printf("%i\n", pkg);
-  // print_data_package(&pkg, PACKAGE_PAYLOAD_LENGTH);
+    short_package acknowlegement = {0xFF, pck->seq};
+
+    printf("Sending Acknowlegement with SEQ %i.\n", pck->seq);
+    simple_udp_sendto(&udp_conn, &acknowlegement, 2, sender_addr);
+  } else {
+    printf("Received Acknowlegement with SEQ %i.\n", pck->seq);
+  }
 }
+
 
 PROCESS_THREAD(receiver_process, ev, data)
 {
     PROCESS_BEGIN();
 
-    NETSTACK_RADIO.set_value(RADIO_PARAM_RX_MODE, 1);
-    NETSTACK_RADIO.init();
-    NETSTACK_RADIO.on();
-
-    int packages_received = 0;
-
-    while(1) {
-      while(!NETSTACK_RADIO.pending_packet());
-      printf("#### %i ####\n", packages_received);
-      rx_callback();
-      packages_received++;
-    }
+    LOG_INFO("registering UDP connection\n");
+    NETSTACK_ROUTING.root_start();
+    simple_udp_register(&udp_conn, SENDER_PORT, NULL, RECEIVER_PORT, udp_rx_callback);
+    LOG_INFO("registered UDP connection\n");
 
     PROCESS_END();
 }
