8646b1b169221a9c758ab90256ae9928336b4675
Konstantin Kochs <konstantin.kochs@mailbox.tu-dresden.de>
2023-09-01 14:34:48 +0200

    receiver with hello packet

--- src/receiver.c
+++ src/receiver.c
@@ -32,18 +32,41 @@ static void udp_rx_callback(struct simple_udp_connection *c,
 
 PROCESS_THREAD(receiver_process, ev, data)
 {
+  static struct etimer periodic_timer;
+
   PROCESS_BEGIN();
 
-  LOG_INFO("registering UDP connection\n");
+  uip_ipaddr_t root_ip;
+  etimer_set(&periodic_timer, 2*CLOCK_SECOND);
 
-  NETSTACK_ROUTING.root_start();
+  // NETSTACK_ROUTING.root_start();
+
+  while (true) {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+
+    if (NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&root_ip)) {
+
+      LOG_INFO("registering UDP connection\n");
+      simple_udp_register(&udp_conn, LOCAL_PORT, &root_ip, REMOTE_PORT, udp_rx_callback);
+
+      break;
+    }
+
+    etimer_reset(&periodic_timer);
+  }
+
+  LOG_INFO("######## registered ...\n");
+  LOG_INFO("root_ip ");
+  LOG_INFO_6ADDR(&root_ip);
+  LOG_INFO("\n");
+
+  // etimer_reset_with_new_interval(&periodic_timer, 5*CLOCK_SECOND);
+  // PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+
+  short_package hello_pkg = {0x00, (char) -1};
+  simple_udp_send(&udp_conn, &hello_pkg, 2);
 
   init_print_full_log();
-  do {
-    simple_udp_register(&udp_conn, LOCAL_PORT, NULL, REMOTE_PORT, udp_rx_callback);
-
-    LOG_INFO("######## registered ...");
-  } while (!NETSTACK_ROUTING.node_is_reachable());
-
+  
   PROCESS_END();
 }
