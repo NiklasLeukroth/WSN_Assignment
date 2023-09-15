bc3581592b9acda0bb662e15df13e8a40bbb1c69
Konstantin Kochs <konstantin.kochs@mailbox.tu-dresden.de>
2023-09-15 12:02:13 +0200

    adapted wsn_gobal

--- src/receiver.c
+++ src/receiver.c
@@ -8,18 +8,11 @@
 static struct simple_udp_connection udp_conn;
 
 PROCESS(receiver_process, "receiver process");
-PROCESS(receiver_connect, "receiver connect process");
 AUTOSTART_PROCESSES(&receiver_process);
 
 struct etimer receiver_timeout;
 
-static void udp_rx_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
+static void udp_rx_callback()
 {
   etimer_reset(&receiver_timeout);
 
@@ -33,62 +26,20 @@ static void udp_rx_callback(struct simple_udp_connection *c,
   }
 }
 
-PROCESS_THREAD(receiver_connect, ev, data)
-{
-  static struct etimer periodic_timer;
-
-  PROCESS_BEGIN();
-
-  uip_ipaddr_t root_ip;
-  etimer_set(&periodic_timer, 2*CLOCK_SECOND);
-
-  while (true) {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-
-    if (NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&root_ip)) {
-
-      LOG_INFO("registering UDP connection\n");
-      simple_udp_register(&udp_conn, LOCAL_PORT, &root_ip, REMOTE_PORT, udp_rx_callback);
-
-      break;
-    }
-
-    etimer_reset(&periodic_timer);
-  }
-
-  LOG_INFO("######## registered ...\n");
-  LOG_INFO("root_ip ");
-  LOG_INFO_6ADDR(&root_ip);
-  LOG_INFO("\n");
-
-  PROCESS_END();
-}
-
 
 PROCESS_THREAD(receiver_process, ev, data)
 {
   PROCESS_BEGIN();
 
-  process_start(&receiver_connect, NULL);
-
-  short_package hello_pkg = {0x00, (char) -1};
-  simple_udp_send(&udp_conn, &hello_pkg, 2);
-  etimer_set(&receiver_timeout, 2*CLOCK_SECOND);
-
-  init_print_full_log();
+  wsn_radio.init();
+  wsn_radio.on();
 
   while (true)
   {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&receiver_timeout));
-
-    if (!NETSTACK_ROUTING.node_is_reachable()) {
-      LOG_INFO("Lost connection. Reestablishing ...");
-      process_start(&receiver_connect, NULL);
-    }
-
-    etimer_reset(&receiver_timeout);
+    PROCESS_YIELD_UNTIL(wsn_radio.pending_packet());
   }
   
+
   
   PROCESS_END();
 }
--- src/wsn_global.h
+++ src/wsn_global.h
@@ -6,11 +6,9 @@
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define ZOUL_CONF_USE_CC1200_RADIO 0
+#include "radio/cc1200/cc1200.c"
 
-#include "net/routing/routing.h"
-#include "net/netstack.h"
-#include "net/ipv6/simple-udp.h"
+radio_driver wsn_radio = cc1200_driver;
 
 #define PACKAGE_PAYLOAD_LENGTH 30
 
@@ -43,15 +41,15 @@ void init_print_full_log() {
 
 void print_full_log(data_package *pkg, uint16_t datalen, long timestamp) {
   radio_value_t rssi, lqi;
-  if (timestamp == -1 && NETSTACK_RADIO.get_object(RADIO_PARAM_LAST_PACKET_TIMESTAMP, &timestamp, sizeof(long)) != RADIO_RESULT_OK) {
+  if (timestamp == -1 && wsn_radio.get_object(RADIO_PARAM_LAST_PACKET_TIMESTAMP, &timestamp, sizeof(long)) != RADIO_RESULT_OK) {
     timestamp = 0xFFFF;
   }
 
-  if (NETSTACK_RADIO.get_value(RADIO_PARAM_LAST_RSSI, &rssi) != RADIO_RESULT_OK) {
+  if (wsn_radio.get_value(RADIO_PARAM_LAST_RSSI, &rssi) != RADIO_RESULT_OK) {
     rssi = 0xFFFF;
   }
 
-  if (NETSTACK_RADIO.get_value(RADIO_PARAM_LAST_LINK_QUALITY, &lqi) != RADIO_RESULT_OK) {
+  if (wsn_radio.get_value(RADIO_PARAM_LAST_LINK_QUALITY, &lqi) != RADIO_RESULT_OK) {
     lqi = 0xFFFF;
   }
 
